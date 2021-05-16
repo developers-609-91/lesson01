a811f875df9e890dd805ae0c82a57922cc878c50
PRYAMOI <usarov212@gmail.com>
2021-05-16 22:33:15 +0500

    сделал с Хитуном, я сделла внемний вид, а он писал скрипты

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var mainViewModel = new MainViewModel();
+            BindingContext = mainViewModel;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            AddBtn.Background = clickedButton.Background;
+        }
+    }
+}
+
